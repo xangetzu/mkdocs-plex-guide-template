8a4d5e6e36b7587004cecb947f313d993c87a702
Kevin C. Moore <kevin@sbotnas.io>
2025-02-16 01:47:55 +0000

    More macros tests

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
+from datetime import datetime
 
 def define_env(env):
     """
     This hook is called when the config is loading.
     """
-    # Get environment variables with fallbacks
     username = os.getenv('GITHUB_REPOSITORY_OWNER', 'Ghost')
     repo_name = os.getenv('GITHUB_REPOSITORY', 'mkdocs-plex-guide-template').split('/')[-1]
+    current_year = datetime.now().year
 
-    # Add them to the macros environment
     env.variables.update({
         "username": username,
         "repo_name": repo_name,
+        "year": current_year,
         "request_url": "request.example.com",
         "domain": "example.com"
     })
